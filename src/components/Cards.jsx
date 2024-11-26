4608d8df114324317acb557db5eccfc703aba524
mohitgoyat525 <montyits71@gmail.com>
2024-11-26 12:58:52 +0530

    add-chnages

---
+++ src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import madara from '../assets/images/png/madara.jpg'
+const Cards = () => {
+  return (
+      <>
+          <div className='flex items-center justify-center '>
+          <div className='border border-solid border-black rounded-xl w-full max-w-[450px] p-5 '>
+              <img src={madara} alt="madra" className='w-full rounded-lg max-w-[400px]' />
+              <h2 className='text-4xl font-semibold text-center pt-3'>Ghost of Uchhia Madara Uchhia</h2>
+              <p className='text-2xl font-medium italic text-center pt-3'>Wake up to reality nothing ever goes as you plan in this curse world </p>
+
+              </div>
+              </div>
+      </>
+  )
+}
+
+export default Cards
